cd793f7c15e1f140c8474c6bb305c6dd4aa33be5
BlockLune <39331194+BlockLune@users.noreply.github.com>
2024-08-12 14:35:14 +0800

    fix: add a `@NonNull` annotation to avoid warning

--- todo-app-springboot/src/main/java/top/fishg/todoapp/config/CorsConfiguration.java
+++ todo-app-springboot/src/main/java/top/fishg/todoapp/config/CorsConfiguration.java
@@ -2,6 +2,7 @@ package top.fishg.todoapp.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -10,7 +11,7 @@ public class CorsConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
-            public void addCorsMappings(CorsRegistry registry) {
+            public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins("http://localhost:3000")
                         .allowedMethods("*");
